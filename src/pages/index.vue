4136352be1a1eb9abec2cd759a21b5cc357768d7
lavaine-iris <iris.lavaine@edu.univ-fcomte.fr>
2022-10-26 08:07:47 +0200

    Intégration page d'accueil

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <h3 class="text-black font-Oswald">PERSONNALISEZ VOTRE MONTRE CONNECTÉE</h3>
+  <div class="bg-Noir1">
+    <div>
+      <h3 class="text-white font-Oswald text-center p-5 text-4xl">PERSONNALISEZ VOTRE MONTRE CONNECTÉE</h3>
+    </div>
+
+    <div>
+      <img class="w-96" src="/public/Montres.png" alt="Montres"/>
+    </div>
+
+    <div>
+      <p class="text-white text-2xl">Les montres connectées unisexe sont très en vogue ! Elles permettent à la fois de recevoir les notifications du téléphone directement sur sa montre (appels, messages, réseaux sociaux) mais elle est aussi un atout sportif (podomètre, calories…). Tik-Tak vous propose une personnalisation complète des couleurs de votre montre (bracelet, boîtier, écran).</p>
+      <h4 class="text-white font-Oswald text-3xl m-5">OFFRE DE LANCEMENT : 239€ AU LIEU DE 299€ !</h4>
+      <button class="bg-white py-4 px-20 font-Oswald font-semibold">PROFITEZ-EN</button>
+    </div>
+
+    <div class="m-5">
+      <router-link to="/personnaliser">
+        <button class="bg-white py-4 px-20 font-Oswald font-semibold">PERSONNALISER</button>
+      </router-link>
+    </div>
+
+    <div>
+      <h3 class="text-white font-Oswald text-center p-5 text-4xl">MODÈLES D’INSPIRATIONS</h3>
+      <img class="" src="/public/Montres-inspi.png" alt="Inspirations montres"/>
+    </div>
+
+    <div>
+      <h3 class="text-white font-Oswald text-center p-5 text-4xl">AVIS CLIENTS</h3>
+    </div>
   </div>
 </template>
