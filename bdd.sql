a9e8814c9985018c8484cfc48131160cde5f86f1
lavaine-iris <iris.lavaine@edu.univ-fcomte.fr>
2022-10-27 10:35:00 +0200

    Code création des views

--- bdd.sql
+++ bdd.sql
@@ -12,10 +12,22 @@
 --
 -- code pour la création des vues
 --
+CREATE VIEW allCuirs as
+select "Montre".*
+from "Matériaux", "Montre"
+where "Matériaux"."Id" = "Montre".id_mat_boitier or "Matériaux"."Id" = "Montre".id_mat_bracelet
+and "Matériaux"."Libelle" = 'Cuir';
 
+CREATE VIEW allMatériaux as
+SELECT *
+FROM "Matériaux";
 
-
-
+create view allMontre as
+SELECT *
+FROM "Montre";
+(uid() = id_user)
+(uid() IN ( SELECT allMontre.id_user
+   FROM allMontre))
 --
 -- code pour la création des policies
 --
